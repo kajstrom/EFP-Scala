85bf89a186899089213c5d5f2cfa4ace76b7a8b3
Kaj Ström <kaj.strom@kapsi.fi>
2017-07-15 12:10:50 +0300

    Exercise 56: Loading items from file

--- src/main/scala/fi/kajstrom/efpscala/E56/InventoryService.scala
+++ src/main/scala/fi/kajstrom/efpscala/E56/InventoryService.scala
@@ -7,9 +7,12 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import fi.kajstrom.efpscala.Util.FilePath
 
+import scala.io.Source
+
 class InventoryService {
-  private var inventory = List[InventoryItem]()
   private val storagePath = Paths.get(FilePath.makeResourcePath("E56_inventory.json"))
+  private var inventory = loadInventory()
+  println(inventory)
 
   def add(item: InventoryItem): Unit = {
     inventory = inventory :+ item
@@ -20,6 +23,20 @@ class InventoryService {
     val json = Json.toJson(inventory)
     new PrintWriter(storagePath.toString) { write(Json.stringify(json)); close }
   }
+
+  private def loadInventory(): List[InventoryItem] = {
+    if (Files.exists(storagePath)) {
+      val itemJson = Source.fromFile(storagePath.toString).mkString
+      val json = Json.parse(itemJson)
+
+      json.validate[List[InventoryItem]] match {
+        case s: JsSuccess[List[InventoryItem]] => s.get
+        case e: JsError => List[InventoryItem]()
+      }
+    } else {
+      List[InventoryItem]()
+    }
+  }
 }
 
 object InventoryItem {
@@ -28,6 +45,12 @@ object InventoryItem {
       (JsPath \ "serialNumber").write[String] and
       (JsPath \ "value").write[Float]
     )(unlift(InventoryItem.unapply))
+
+  implicit val inventoryItemReads: Reads[InventoryItem] = (
+      (JsPath \ "name").read[String] and
+      (JsPath \ "serialNumber").read[String] and
+      (JsPath \ "value").read[Float]
+  )(InventoryItem.apply _)
 }
 
 case class InventoryItem(name: String, serialNumber: String, value: Float) {}
